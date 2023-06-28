511da23a4c91222cd8fb1717e02c43d3e9cf5c8b
emderrick <92058648+emderrick@users.noreply.github.com>
2023-06-28 09:47:45 -0400

    updates

--- fisher_test.R
+++ fisher_test.R
@@ -1,2 +1,14 @@
 library(tidyverse)
 library(dplyr)
+
+epsps_sns <- data.frame("increase" = c(3,2),
+                        "decrease" = c(2,1),
+                        row.names = c("Class I", "Class II"))
+
+fish_test_sns<- fisher.test(epsps_sns, alternative = "less")
+
+epsps_snv <- data.frame("increase" = c(1,1),
+                        "decrease" = c(4,2),
+                        row.names = c("Class I", "Class II"))
+
+fish_test_snv<- fisher.test(epsps_snv)
