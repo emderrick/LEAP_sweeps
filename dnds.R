fbdee8a9c5a9e8210f05a248db0870d9bd978717
emderrick <92058648+emderrick@users.noreply.github.com>
2023-08-15 12:35:26 -0400

    update files

--- dnds.R
+++ dnds.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(ggplot2)
 library(ggpubr)
 library(stringr)
-library(patchwork)
+library(cowplot)
 
 all_MAG_snvs <- read_csv("all_MAG_SNVs_med_Aug8.csv")
 all_MAG_snvs <- subset(all_MAG_snvs, str_detect(new_name, "T2"))
@@ -12,11 +12,18 @@ MAG_snvs <- all_MAG_snvs[, c('mag', 'gene', 'mutation_type', 'new_name')] %>% su
 MAG_snvs$mutation <- 1
 MAG_snvs_sum <- MAG_snvs %>% group_by(mag, new_name) %>% summarise(total = sum(is.na(mutation_type)==F))
 MAG_snvs_all <- left_join(MAG_snvs, MAG_snvs_sum, by = c('mag', 'new_name'))
-MAG_dnds_by_gene <- MAG_snvs %>% group_by(gene, new_name) %>% summarize(N = sum(mutation_type=="N"), S = (mutation_type=="S"))
+MAG_snvs_all$graph_name <- str_sub(MAG_snvs_all$new_name, end = -6) 
+MAG_snvs_all$graph_name <- gsub('Control', 'Ctrl', MAG_snvs_all$graph_name)
 
-dnds_plot <- ggplot(MAG_snvs_all, aes(x=new_name, y=mutation/total, fill=mutation_type))+
-  geom_bar(stat="identity")+
+MAG_dnds_by_gene <- MAG_snvs %>% group_by(gene, new_name) %>% summarize(N = sum(mutation_type=="N"), S = sum(mutation_type=="S")) %>% 
+  pivot_longer(names_to = "mutation_type", values_to = "count")
+
+dnds_plot <- ggplot(MAG_snvs_all, aes(x = graph_name, y = mutation/total, fill = mutation_type))+
+  geom_bar(stat = "identity")+
   theme_classic()+
-  labs(y= "Fraction of SNVs", x = "pond", legend = "Mutation Type")+
-  facet_wrap(~mag, nrow=4, ncol=4, scales="free")
-ggsave("dnds_MAG_plot.png", limitsize = F, dpi = 200)
+  labs(y = "Fraction of SNVs", x = "pond", legend = "Mutation Type")+
+  facet_wrap(~mag, nrow = 4, ncol = 4, scales="free")
+
+save_plot("dnds_MAG_plot.jpeg", dnds_plot, ncol = 4, nrow = 4, dpi = 300)
+ggsave("dnds_MAG_plot.png", limitsize = F, dpi = 600)
+
--- graphing_coverage.R
+++ graphing_coverage.R
@@ -3,166 +3,25 @@ library(ggplot2)
 library(dplyr)
 library(viridis)
 library(stringr)
-library(ggpubr)
+library(cowplot)
 
-setwd("/Users/Emma/OneDrive - McGill University/Documents/phd docs/Chapter 1/Aim 1A")
-
-mag_list<-list("L3_MAG_00058", "I8_MAG_00005", "L4_MAG_00099", "L8_MAG_00019", "L8_MAG_00011",
-               "L7_MAG_00043", "L7_MAG_00028", "I4_MAG_00006", "I4_MAG_00065", "L2_MAG_00052",
-               "L7_MAG_00020", "L8_MAG_00042", "L2_MAG_00048")
+mag_list <- list("I4_MAG_00006", "I4_MAG_00065", "L2_MAG_00052", "L3_MAG_00058", "L4_MAG_00099",
+                  "L7_MAG_00020", "L7_MAG_00028", "L7_MAG_00043", "L8_MAG_00011", "L8_MAG_00019", "L8_MAG_00042")
+              
 #load mag snv info
-mags <- read_csv("filtered_ANI_95_mag_SNVs.csv")
+all_snv <- read_csv("filtered_ANI_95_mag_SNVs.csv")
+scaffold_info <- all_snv[, c('length', 'scaffold', 'coverage', 'new_name', 'mag')] %>% distinct()
+mag_cov<- all_snv %>% group_by(scaffold, new_name) %>% summarize(SNV_SNS_tot = sum(number_divergent))
+mag_cov <- left_join(mag_cov, scaffold_info, by = c("scaffold", "new_name"))
 
-#sum up number of SNVs by scaffold
-mag_cov <- aggregate(mags$number_SNVs, by=list(mag=mags$mag, time=mags$new_time, scaffold=mags$scaffold, length=mags$length, group=mags$new_name, coverage=mags$coverage), FUN=sum) 
-mag_cov <- mag_cov %>% rename("SNVs"="x")
-mag_cov$snvbylength <- mag_cov$SNVs/mag_cov$length
-
-#scaffold coverage for all together
-ggplot(mag_cov, aes(x = coverage, y=snvbylength*10^6, group=scaffold)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)")+
-  theme_classic() +
-  theme(legend.title=element_blank())
-ggsave("all_scaffold_coverage.png", limitsize = FALSE, dpi=500) 
-
-#individual coverage graphs
-L7_MAG_00028_cov <- ggplot(subset(mag_cov, mag == "L7_MAG_00028"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
+all_MAG_cov <-  ggplot(mag_cov, aes(x = coverage, y=(SNV_SNS_tot/length)*10^6, colour=new_name)) + 
+  geom_point()+
   scale_colour_viridis(discrete = T, option= "magma")+
   labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L7_MAG_00028")+
-  theme(plot.title = element_text(hjust = 0.5))+
   theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L7_MAG_00028_cov.png", limitsize = FALSE)
-
-L8_MAG_00011_cov <- ggplot(subset(mag_cov, mag == "L8_MAG_00011"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L8_MAG_00011")+
   theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L8_MAG_00011_cov.png", limitsize = FALSE)
+  theme(legend.title=element_blank())+
+  facet_wrap(~mag, nrow = 4, ncol = 4, scales="free")
 
-L8_MAG_00019_cov <- ggplot(subset(mag_cov, mag == "L8_MAG_00019"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L8_MAG_00019")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L8_MAG_00019_cov.png", limitsize = FALSE)
-
-L7_MAG_00043_cov <-  ggplot(subset(mag_cov, mag == "L7_MAG_00043"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L7_MAG_00043")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L7_MAG_00043_cov.png", limitsize = FALSE)
-
-L3_MAG_00058_cov <-  ggplot(subset(mag_cov, mag == "L3_MAG_00058"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L3_MAG_00058")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L3_MAG_00058_cov.png", limitsize = FALSE)
-
-I8_MAG_00005_cov <-  ggplot(subset(mag_cov, mag == "I8_MAG_00005"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("I8_MAG_00005")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="I8_MAG_00005_cov.png", limitsize = FALSE)
-
-L4_MAG_00099_cov <- ggplot(subset(mag_cov, mag == "L4_MAG_00099", time="2"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L4_MAG_00099")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L4_MAG_00099_cov.png", limitsize = FALSE)
-
-I4_MAG_00006_cov <-  ggplot(subset(mag_cov, mag == "I4_MAG_00006", time), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("I4_MAG_00006")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="I4_MAG_00006_cov.png", limitsize = FALSE)
-
-I4_MAG_00065_cov <-  ggplot(subset(mag_cov, mag == "I4_MAG_00065"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("I4_MAG_00065")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="I4_MAG_00065_cov.png", limitsize = FALSE)
-
-L2_MAG_00052_cov <-  ggplot(subset(mag_cov, mag == "L2_MAG_00052"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L2_MAG_00052")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L2_MAG_00052_cov.png", limitsize = FALSE)
-
-L7_MAG_00020_cov <-  ggplot(subset(mag_cov, mag == "L7_MAG_00020"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L7_MAG_00020")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L7_MAG_00020_cov.png", limitsize = FALSE)
-
-L8_MAG_00042_cov <-  ggplot(subset(mag_cov, mag == "L8_MAG_00042", time="2"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L8_MAG_00042")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L8_MAG_00042_cov.png", limitsize = FALSE)
-
-L2_MAG_00048_cov <-  ggplot(subset(mag_cov, mag == "L2_MAG_00048"), aes(x = coverage, y=snvbylength*10^6)) + 
-  geom_point(aes(colour=group))+
-  scale_colour_viridis(discrete = T, option= "magma")+
-  labs(y="SNVs / MBp", x="Coverage (x)") +
-  ggtitle("L2_MAG_00048")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme_classic()+
-  theme(legend.title=element_blank())
-ggsave(filename="L2_MAG_00048_cov.png", limitsize = FALSE)
-
-
-ggarrange(L3_MAG_00058_cov, I8_MAG_00005_cov, L4_MAG_00099_cov, L8_MAG_00019_cov, L8_MAG_00011_cov,
-          L7_MAG_00043_cov, L7_MAG_00028_cov, I4_MAG_00006_cov, I4_MAG_00065_cov, 
-          L7_MAG_00020_cov, L8_MAG_00042_cov, L2_MAG_00048_cov, L2_MAG_00052_cov,
-          labels = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"),
-          ncol = 3, nrow = 5)
-ggsave(filename="95_individual_cov.png", limitsize = FALSE, width=16, height=10)
+save_plot("MAG_cov_SNV.jpeg", all_MAG_cov, ncol = 4, nrow = 4, dpi = 300)
 
