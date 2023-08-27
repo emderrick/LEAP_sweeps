4d4f7dabef745e57237c479345b84da23b408913
emderrick <92058648+emderrick@users.noreply.github.com>
2023-08-27 14:58:26 -0400

    update files

--- NS_ratio.R
+++ NS_ratio.R
@@ -12,14 +12,27 @@ mag_labs <- c(I4_MAG_00006 = "SJAQ100 sp016735685 assembled from Control B", I4_
 
 all_MAG_snvs <- read_csv("all_MAG_SNVs_med_Aug15.csv")
 all_MAG_snvs <- subset(all_MAG_snvs, str_detect(new_name, "T2"))
-MAG_snvs <- all_MAG_snvs[, c('mag', 'gene', 'mutation_type', 'new_name')] %>% subset(is.na(mutation_type) == F)
+MAG_snvs <- all_MAG_snvs[, c('mag', 'gene', 'mutation_type', 'new_name', 'mag_length')] %>% subset(is.na(mutation_type) == F)
 MAG_snvs$mutation <- 1
 
-MAG_NS <- MAG_snvs %>% group_by(mag, new_name) %>% summarize(N = sum(mutation_type=="N"), S = sum(mutation_type=="S"), I = sum(mutation_type=="I"), M = sum(mutation_type=="M"))
+MAG_NS <- MAG_snvs %>% group_by(mag, mag_length, new_name) %>% summarize(N = sum(mutation_type=="N"), S = sum(mutation_type=="S"), I = sum(mutation_type=="I"), M = sum(mutation_type=="M"))
 MAG_NS$total <- MAG_NS$N + MAG_NS$S + MAG_NS$I + MAG_NS$M
 MAG_NS$graph_name <- str_sub(MAG_NS$new_name, end = -6) 
 MAG_NS$graph_name <- gsub('Control', 'Ctrl', MAG_NS$graph_name)
 MAG_NS_long <- pivot_longer(MAG_NS, cols = c("N", "S", "I", "M"), names_to = "mutation_type", values_to = "count")
+MAG_NS$NS_ratio <-  MAG_NS$N / MAG_NS$S
+MAG_NS$SNVs_MBp <- (MAG_NS$total / MAG_NS$mag_length) * 1000000
+MAG_NS$treatment <- str_sub(MAG_NS$new_name, end = -8)
+MAG_NS$diversity <- with(MAG_NS, ifelse(SNVs_MBp < 2000, 'low', 'high'))
+MAG_NS_wide <- MAG_NS[, c('mag', 'new_name', 'SNVs_MBp')] %>% pivot_wider(names_from = new_name, values_from = SNVs_MBp)
+MAG_NS_wide <- MAG_NS_wide %>% rowwise() %>% mutate(max_SNVs_mag = max(c_across(contains("T")), na.rm = T))
+MAG_NS_wide <- MAG_NS_wide %>% rowwise() %>% mutate(max_SNVs_mag_ctl = max(c_across(contains("Control")), na.rm = T))
+MAG_NS_plotting <- pivot_longer(MAG_NS_wide, cols = contains("T2"), names_to = 'new_name', values_to = 'SNVs_MBp') %>% subset(is.na(SNVs_MBp) == F) 
+MAG_NS_plotting <- subset(MAG_NS_plotting, SNVs_MBp == max_SNVs_mag_ctl)
+MAG_NS_plotting <- right_join(MAG_NS, MAG_NS_plotting, by = c('mag', 'new_name', 'SNVs_MBp'))
+
+
+#graphing
 
 NS_plot <- ggplot(MAG_NS_long, aes(x = graph_name, y = count/total, fill = mutation_type))+
   geom_bar(stat = "identity")+
@@ -28,6 +41,13 @@ NS_plot <- ggplot(MAG_NS_long, aes(x = graph_name, y = count/total, fill = mutat
   facet_wrap(~mag, nrow = 4, ncol = 4, scales="free", labeller = labeller(mag = mag_labs))
 
 save_plot("NS_MAG_plot.jpeg", NS_plot, ncol = 4, nrow = 4, dpi = 300)
+
+NS_SNV_plot <- ggplot(MAG_NS_plotting, aes(x = NS_ratio, y = SNVs_MBp, colour = diversity))+
+  geom_point()+
+  labs(y = "SNVs / MBp", x = "N/S ratio", colour = "Diversity") +
+  theme_classic()
+
+save_plot("NS_SNV_plot.jpeg", NS_SNV_plot)
 
 
 # look at N:S ratio by gene
--- sweeps.R
+++ sweeps.R
@@ -17,8 +17,8 @@ write.csv(MAG_sweep_wide, "all_MAG_sweep_wide.csv")
 
 MAG_sweep_wide$mag <- MAG_sweep_wide$groups %>% substr(1,12)
 MAG_sweep_wide$position <- MAG_sweep_wide$groups %>% substr(27,33) %>% str_remove("^0+")
-MAG_sweep_wide$control_mean <- rowMeans(MAG_sweep_wide[,c('Control A at T2', 'Control B at T2', 'Control C at T2', 'Control D at T2', 'Control E at T2')], na.rm = T)
-MAG_sweep_wide$GBH_mean <- rowMeans(MAG_sweep_wide[,c('GBH A at T2', 'GBH B at T2', 'GBH C at T2', 'GBH D at T2')], na.rm=T)
+MAG_sweep_wide$control_mean <- rowMeans(MAG_sweep_wide[, c('Control A at T2', 'Control B at T2', 'Control C at T2', 'Control D at T2', 'Control E at T2')], na.rm = T)
+MAG_sweep_wide$GBH_mean <- rowMeans(MAG_sweep_wide[, c('GBH A at T2', 'GBH B at T2', 'GBH C at T2', 'GBH D at T2')], na.rm=T)
 MAG_sweep_wide$abs_val <- abs(MAG_sweep_wide$control_mean - MAG_sweep_wide$GBH_mean)
 MAG_sweep_wide$abs_val <- with(MAG_sweep_wide, ifelse(is.nan(abs_val), NA, abs_val))
 
@@ -27,9 +27,11 @@ threshold_snvs <- read_csv("threshold_snvs.csv")
 MAG_sweep_wide <- left_join(MAG_sweep_wide, threshold_snvs[,c('groups', 'pass')])
 MAG_sweep_wide$pass <- with(MAG_sweep_wide, ifelse(is.na(pass), 'no', pass))
 
-scaffold_info <- read.csv("ANI_95_all_scaffolds.csv") #some scaffolds are missing the length (ones that weren't in SNV file)
+scaffold_info <- read.csv("ANI_95_all_scaffolds.csv") 
 scaffold_info <- scaffold_info[,c('scaffold', 'length')] %>% distinct()
-unique_sweep <- left_join(MAG_sweep_wide[,c('scaffold', 'mag')], scaffold_info, by = "scaffold") %>% group_by(scaffold) %>% fill(length, .direction = "updown") %>% distinct() 
+unique_sweep <- left_join(MAG_sweep_wide[, c('scaffold', 'mag')], scaffold_info, by = "scaffold") %>% group_by(scaffold) %>% fill(length, .direction = "updown") %>% distinct() 
+#there is a scaffold missing in this (cov must have been low) so I had to manually get the length from the fasta file of the MAG with "cat L3_MAG_00058.fa | grep -A1 "L3_MAG_00058_000000000198" | grep -v ">L3_MAG_00058_000000000198" | wc -c" = 15931
+unique_sweep$length[unique_sweep$scaffold == "L3_MAG_00058_000000000198"] <- 15931
 unique_sweep <- unique_sweep %>% group_by(mag) %>% arrange(desc(length), .by_group = T)
 
 unique_sweep_new_pos <- data.frame()
@@ -43,11 +45,12 @@ for(MAG in mag_list){
   unique_sweep_new_pos <- bind_rows(unique_sweep_new_pos, unique_MAG)
 }
 
-MAG_sweep_plot <- left_join(MAG_sweep_wide[,c('mag', 'scaffold', 'position', 'groups', 'abs_val', 'pass')], unique_sweep_new_pos[,c('scaffold', 'scaffold_position')], by = 'scaffold')
+MAG_sweep_plot <- left_join(MAG_sweep_wide[, c('mag', 'scaffold', 'position', 'groups', 'abs_val', 'pass')], unique_sweep_new_pos[,c('scaffold', 'scaffold_position')], by = 'scaffold')
 MAG_sweep_plot$abs_val_pass <- with(MAG_sweep_plot, ifelse(pass=="yes", abs_val, NA))
 write.csv(MAG_sweep_plot, "MAG_sweep_plot.csv")
 
-test <- MAG_sweep_plot %>% group_by(scaffold) %>% colMeans(abs_val, nrow=1000)
+I4_MAG_00065 <- subset(MAG_sweep_plot, scaffold=="I4_MAG_00065_000000000011")
+I4_MAG_00065$test <- colMeans(matrix(I4_MAG_00065$abs_val, nrow=6), na.rm=T)
 
 L8_MAG_00011_sweep <- ggplot(subset(MAG_sweep_plot, mag == "L8_MAG_00011"), aes(x = reorder(groups, scaffold_position), y = abs_val, group = mag))+
   geom_line()+
