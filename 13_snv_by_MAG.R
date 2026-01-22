25cca5a6cdf793a120869d77f7dab34e3b8641db
emderrick <92058648+emderrick@users.noreply.github.com>
2026-01-22 13:07:30 -0500

    Update 13_snv_by_MAG.R

--- 13_snv_by_MAG.R
+++ 13_snv_by_MAG.R
@@ -9,13 +9,8 @@ all_samples <- read_csv("data files/chapter_1_sample_names.csv")
 all_samples$Time <- ifelse(grepl("_1", all_samples$Pond), "Day 0", "Day 28")
 all_samples <- subset(all_samples, !(Name == "CTRL E"))
 
-all_snv <- read_csv("data files/MAG_SNV_depth_info.csv")
-all_snv$Time <- ifelse(grepl("1", all_snv$time), "Day 0", "Day 28")
-all_snv$Treatment <- ifelse(grepl("CTRL", all_snv$Name), "Control", "GBH")
-mag_snv <- subset(all_snv, mag_coverage >= 5 & mag_breadth >= 0.7)
-
-all_sum <- read_csv("data files/T1_SNV_summary_MAG.csv")
-all_sum <- subset(all_sum, mag %in% mag_list & mag_coverage >= 5 & mag_breadth >= 0.7)
+all_sum <- read_csv("data files/T1_subsamp_SNV_summary_MAG.csv")
+all_sum <- subset(all_sum, mag %in% mag_list)
 all_sum$Name_Time <- paste(all_sum$Name, all_sum$Time, sep = " ")
 
 plot_labels <- data.frame(mag = c(unique(all_sum$mag)),
@@ -52,48 +47,7 @@ sns_mag <- ggplot(all_sum, aes(x = Name, y = SNSs_Mbp, fill = Treatment))+
   facet_grid(species ~ Time, scales = "free", labeller = labeller(species = label_wrap_gen(width = 20)))
 
 mag_plot <- ggarrange(snv_mag, sns_mag, common.legend = T, legend = "none", ncol = 2, labels = c("A", "B"))
-ggsave("mag_sns_snv.pdf", mag_plot, units = "cm", width = 17, height = 20)
+ggsave("figures/mag_sns_snv.pdf", mag_plot, units = "cm", width = 17, height = 20)
 
 all_sum %>% group_by(species, mag, Time) %>% count()
 
-# for(i in 1:length(mag_list)){
-#   bin_sum <- subset(all_sum, mag == mag_list[i])
-#   bin_sum <- full_join(bin_sum, all_samples[, c(1,3,4)])
-#   bin_sum$Treatment <- ifelse(grepl("CTRL", bin_sum$Name), "Control", "GBH")
-#   
-#   bin_snv <- subset(mag_snv, mag == mag_list[i])
-#   bin_snv <- full_join(bin_snv, all_samples[, c(1,3,4)])
-#   bin_snv <- subset(bin_snv, final_ref_freq < 1 & final_ref_freq > 0)
-#   bin_snv <- bin_snv %>% complete(group, Name, Time)
-#   bin_snv <- bin_snv %>% group_by(Name) %>% fill(Treatment, .direction = "updown")
-#   bin_snv$Name <- factor(bin_snv$Name, levels = c("CTRL A", "CTRL B", "CTRL C", "CTRL D", "GBH A", "GBH B", "GBH C", "GBH D"))
-#   
-#   bin_snv_sum <- ggplot(bin_sum, aes(x = Name, y = SNVs_Mbp, fill = Treatment))+
-#     geom_col()+
-#     theme_classic()+
-#     scale_fill_manual(values = c("darkgreen", "darkmagenta"))+
-#     labs(x = "", y = "Polymorphic sites per Mbp", fill = "")+
-#     theme(text = element_text(size = 8), axis.text = element_text(colour = "black", size = 6), strip.text.x = element_text(size = 8), legend.position = "none")+
-#     facet_wrap(~Time, scales = "fixed")
-#   
-#   bin_sns_sum <- ggplot(bin_sum, aes(x = Name, y = SNSs_Mbp, fill = Treatment))+
-#     geom_col()+
-#     theme_classic()+
-#     scale_fill_manual(values = c("darkgreen", "darkmagenta"))+
-#     labs(x = "", y = "Fixed subsitutions per Mbp", fill = "Treatment")+
-#     theme(text = element_text(size = 8), axis.text = element_text(colour = "black", size = 6), strip.text.x = element_blank(), legend.position = "none")+
-#     facet_wrap(~Time, scales = "fixed")
-#   
-#   bin_snvs <- ggplot(bin_snv, aes(x = Name, y = final_ref_freq, colour = Treatment)) +
-#     geom_boxplot(na.rm = F, outliers = T,outlier.size = 0.5)+
-#     scale_colour_manual(values = c("darkgreen", "darkmagenta"))+
-#     ylim(0,1)+
-#     theme_classic()+
-#     scale_x_discrete(drop = FALSE)+
-#     labs(x = "Pond", y = "Reference frequency of SNVs")+
-#     theme(text = element_text(size = 8), axis.text = element_text(colour = "black", size = 6), strip.text.x = element_blank(), legend.position = "bottom")+
-#     facet_wrap(~Time, scales = "fixed")
-#   
-#   bin_plot <- bin_snv_sum / bin_sns_sum / bin_snvs
-#   ggsave(paste("figures/", mag_list[i], "_snvs.pdf", sep = ""), bin_plot, units = "cm", width = 17, height = 17)
-# }
