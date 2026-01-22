162136546ee7ecc426057dff6ae8da7c90522bf8
emderrick <92058648+emderrick@users.noreply.github.com>
2026-01-22 12:26:46 -0500

    update file

--- 27_aro_genes.R
+++ 27_aro_genes.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 setwd("/Users/emma/Documents/GitHub/LEAP_sweeps/")
 
-
 nonsyn_sig_genes <- read_csv("data files/nonsyn_allele_shifts_07_enriched_genes.csv")
 syn_sig_genes <- read_csv("data files/syn_allele_shifts_07_enriched_genes.csv")
 
@@ -25,7 +24,35 @@ write.csv(aro_genes, "data files/aro_gene_table.csv", row.names = F)
 nonsyn_sig_genes$operon <- nonsyn_sig_genes$Preferred_name %>% substr(1,3)
 nonsyn_operon_sum <- nonsyn_sig_genes %>% group_by(operon) %>% count()
 
-syn_sig_genes$operon <- syn_sig_genes$Preferred_name %>% substr(1,3)
-syn_operon_sum <- syn_sig_genes %>% group_by(operon) %>% count()
+all_genes <- read_csv("data files/cog_background_snv_freq_genes.csv")
+all_genes$operon <- all_genes$Preferred_name %>% substr(1,3)
+all_genes <- subset(all_genes, !(operon == "-"))
+all_genes$operon <- tolower(all_genes$operon)
+all_operon_sum <- all_genes %>% group_by(operon) %>% count()
+colnames(all_operon_sum)[2] <- "total"
+
+operons <- left_join(all_operon_sum, nonsyn_operon_sum)
+operons$ratio <- operons$n / operons$total
+colnames(operons)[3] <- "Nonsynonymous allele shifts"
+operons <- subset(operons, !(nchar(operon) != 3))
+operons <- subset(operons, str_detect(operon, "\\d") == F)
+operons[is.na(operons)] <- 0
+write.csv(operons, "data files/operon_gene_sum.csv", row.names = F)
+
+ggplot(operons, aes(x = total, y = `Nonsynonymous allele shifts`))+
+  geom_point()+
+  theme_bw()+
+  xlim(0,120)+
+  ylim(0,120)+
+  geom_abline(slope = 1)
+
+operons <- subset(operons, ratio > 0)
+ggplot(operons, aes(x = reorder(operon, ratio), y = ratio))+
+  geom_point()+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1, size = 4))
+
+
+
+#operons <- subset(operons, total > 5)
 
 
